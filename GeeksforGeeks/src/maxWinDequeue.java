5c5fe9bf0b703fbf5af4ce278e5d3f270e584825
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-10-05 09:45:16 -0400

    max Sliding win

--- GeeksforGeeks/src/maxWinDequeue.java
+++ GeeksforGeeks/src/maxWinDequeue.java
@@ -40,74 +40,26 @@ class maxWinDequeue {
         }
         System.out.println();
     }
-    static void maxWin_v1(int[] arr, int n, int k)
-    {
-        Deque<Integer> deque = new LinkedList<>();
-        // addLast elements, and at the time of moving window removeFirst (shrink from begining)
-        // unuseful elements that are before i-k window because max is at index i
-        int i = 0;
-        for(; i < k; i++)
-        {
-            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i])
-                deque.pollLast();
-            deque.addLast(i);
-        }
-        for(; i < n; i++)
-        {
-            System.out.print(arr[deque.peekFirst()]+" ");
-
-            while (!deque.isEmpty() && deque.peekFirst() <= i-k)
-                deque.pollFirst();
-
-            while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i])
-                deque.pollLast();
-            deque.addLast(i);
-        }
-        System.out.print(arr[deque.peekFirst()]+" ");
-    }
-    // O(n)
-    // A Dequeue (Double ended queue) based method for printing maximum element of
-    // all subarrays of size k
-    static void maxWin_v1_0(int arr[], int n, int k)
-    {
-        // Create a Double Ended Queue, Qi that will store indexes of array elements
-        // The queue will store indexes of useful elements in every window and it will
-        // maintain decreasing order of values from front to rear in Qi, i.e.,
-        // arr[Qi.front()] to arr[Qi.rear()] are sorted in decreasing order
-        Deque<Integer> Qi = new LinkedList<Integer>();
-
-        /* Process first k (or first window) elements of array */
-        int i;
-        for (i = 0; i < k; ++i) {
-            // For every element, the previous smaller elements are useless so
-            // remove them from Qi
-            while (!Qi.isEmpty() && arr[i] >= arr[Qi.peekLast()])
-                Qi.removeLast(); // Remove from rear
-
-            // Add new element at rear of queue
-            Qi.addLast(i);
-        }
-
-        // Process rest of the elements, i.e., from arr[k] to arr[n-1]
-        for (; i < n; ++i) {
-            // The element at the front of the queue is the largest element of
-            // previous window, so print it
-            System.out.print(arr[Qi.peek()] + " ");
-
-            // Remove the elements which are out of this window
-            while ((!Qi.isEmpty()) && Qi.peek() <= i - k)
-                Qi.removeFirst();
-
-            // Remove all elements smaller than the currently
-            // being added element (remove useless elements)
-            while ((!Qi.isEmpty()) && arr[i] >= arr[Qi.peekLast()])
-                Qi.removeLast();
-
-            // Add current element at the rear of Qi
-            Qi.addLast(i);
-        }
-
-        // Print the maximum element of last window
-        System.out.print(arr[Qi.peek()]);
+    static int[] maxSlidingWindow(int[] arr, int k) {
+        int[] res = new int[arr.length-k+1];
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+	    
+        int j = 0;
+	    for(int i = 0; i < arr.length; i++)
+	    {
+	        while(!dq.isEmpty() && dq.peek() <= i-k)
+	            dq.remove();
+	           
+	        while(!dq.isEmpty() && arr[dq.peekLast()] <= arr[i])
+	            dq.removeLast();
+	        
+	        dq.add(i);
+	        
+	        if(i >= k-1)
+	            res[j++] = arr[dq.peek()];
+	    }
+        
+        return res;
     }
 }
