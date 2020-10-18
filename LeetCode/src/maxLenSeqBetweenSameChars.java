8e02a9024fa338e18d4be9bc92c7d3012b053540
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-10-18 00:23:30 -0400

    LeetCode/src/maxLenSeqBetweenSameChars.java

--- LeetCode/src/maxLenSeqBetweenSameChars.java
+++ LeetCode/src/maxLenSeqBetweenSameChars.java
@@ -1,9 +1,28 @@
-package PACKAGE_NAME;
-
 import java.util.*;
 
+// 5543. Largest Substring Between Two Equal Characters
 public class maxLenSeqBetweenSameChars {
-    public static void main(String[] args)
-    {
+    public int maxLengthBetweenEqualCharacters(String ss) {
+        char[] s = ss.toCharArray();
+        HashMap<Character, int[]> map = new HashMap<>();
+        for (int i = 0; i < s.length; i++) {
+            if (map.containsKey(s[i])) {
+                int[] arr = map.get(s[i]);
+                arr[0] = Math.min(arr[0], i);
+                arr[1] = Math.max(arr[1], i);
+            }
+            else {
+                int[] index = new int[2];
+                index[0] = index[1] = i;
+                map.put(s[i], index);
+            }
+        }
+
+        int d = -1;
+        for (char c : map.keySet()) {
+            int[] indice = map.get(c);
+            d = Math.max(d, indice[1] - indice[0] - 1);
+        }
+        return d;
     }
 }
