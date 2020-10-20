7c968421942bcd599515eed8e279ae4d9ab52a75
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-10-19 21:26:59 -0400

    treeSubTreeOfAnother

---
+++ LeetCode/src/treeSubTreeOfAnother.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class treeSubTreeOfAnother {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    HashSet<String> trees = new HashSet<>();
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        String tree1 = preorder(s, true);
+        String tree2 = preorder(t, true);
+        return tree1.indexOf(tree2) >= 0;
+    }
+    public String preorder(TreeNode t, boolean left) {
+        if (t == null) {
+            if (left)
+                return "lnull";
+            else
+                return "rnull";
+        }
+        return "#" + t.val + " " + preorder(t.left, true) + " " + preorder(t.right, false);
+    }
+
+    public boolean isSubtree_v1(TreeNode s, TreeNode t) {
+        return traverse(s,t);
+    }
+    public boolean equals(TreeNode x,TreeNode y)
+    {
+        if(x==null && y==null)
+            return true;
+        if(x==null || y==null)
+            return false;
+        return x.val==y.val && equals(x.left,y.left) && equals(x.right,y.right);
+    }
+    public boolean traverse(TreeNode s,TreeNode t)
+    {
+        return  s!=null && ( equals(s,t) || traverse(s.left,t) || traverse(s.right,t));
+    }
+}
