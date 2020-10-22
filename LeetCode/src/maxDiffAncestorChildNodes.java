7da83dcb8941ea9cc4abefb55ee23a58cb233ad1
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-10-22 14:06:48 -0400

    LeetCode/src/maxDiffAncestorChildNodes.java

--- LeetCode/src/maxDiffAncestorChildNodes.java
+++ LeetCode/src/maxDiffAncestorChildNodes.java
@@ -1,8 +1,27 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class maxDiffAncestorChildNodes 
-{
-    public static void main(String[] args)
+public class maxDiffAncestorChildNodes {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int maxAncestorDiff(TreeNode root) {
+        return maxDiff(root, -1, 100001);
+    }
+
+    int maxDiff(TreeNode t, int curMax, int curMin)
     {
+        if (t == null) return curMax - curMin;
+
+        return Math.max(maxDiff(t.left, Math.max(t.val, curMax),
+                Math.min(t.val, curMin)),
+                maxDiff(t.right, Math.max(t.val, curMax),
+                        Math.min(t.val, curMin)));
     }
 }
