8573f32c19572302383ec341e0837e7bb9558000
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-10-23 08:00:00 -0400

    minor

--- LeetCode/src/maxSumFirstLastEle.java
+++ LeetCode/src/maxSumFirstLastEle.java
@@ -11,10 +11,11 @@ public class maxSumFirstLastEle {
     }
 
     private static int getMaxSumDp_v0(int[] nums, int K) {
-        int[][][] dp = new int[nums.length][nums.length][K + 1];
+        int n = nums.length;
+        int[][][] dp = new int[n][n][K + 1];
 
-        for (int l = 0; l < nums.length; l++)
-            for (int i = 0; i + l < dp.length; i++)
+        for (int l = 0; l < n; l++)
+            for (int i = 0; i + l < n; i++)
                 for (int k = 1; k <= K; k++)
                     if (l == 0)
                         dp[i][i][k] = nums[i];
@@ -22,7 +23,7 @@ public class maxSumFirstLastEle {
                         dp[i][i + l][k] = Math.max(nums[i] +
                                 dp[i + 1][i + l][k - 1], nums[i + l] + dp[i][i + l - 1][k - 1]);
 
-        return dp[0][nums.length - 1][K];
+        return dp[0][n - 1][K];
     }
 
     private static int getMaxSumDp(int[] nums, int K) {
@@ -33,7 +34,7 @@ public class maxSumFirstLastEle {
                 if (l == 0)
                     dp[i][i] = nums[i];
                 else
-                    dp[i][i + l] = Math.max(nums[i] +
+                    dp[i][i + l] = Math.min(nums[i] +
                             dp[i + 1][i + l], nums[i + l] + dp[i][i + l - 1]);
             }
         }
@@ -43,4 +44,48 @@ public class maxSumFirstLastEle {
 
         return res;
     }
+
+    static int res = Integer.MIN_VALUE;
+
+    private static int getMaxSum(int[] nums, int k) {
+        res = Integer.MIN_VALUE;
+        Deque<Integer> q = new ArrayDeque<>();
+        for(int n : nums) {
+            q.offer(n);
+        }
+        dfs(q, k, 0);
+        return res;
+    }
+
+    private static void dfs(Deque<Integer> q, int k, int tmp) {
+        if(q.isEmpty() || k == 0) {
+            res = Math.max(res, tmp);
+            return;
+        }
+        int first = q.pollFirst();
+        dfs(q, k-1, tmp + first);
+        q.offerFirst(first);
+        int last = q.pollLast();
+        dfs(q, k-1, tmp + last);
+        q.offerLast(last);
+    }
+
+    /*
+    // Memo
+    def largestSumFromEnd(nums, multiply, K):
+    memo = {}
+
+    def dp(i, j, k, sum):
+        if (i, j, k) in memo:
+            return memo[i, j, k]
+        if k == K:
+            return sum
+        else:
+            memo[i, j, k] = max(dp(i + 1, j, k + 1, sum + nums[i] * multiply[k]),
+                                dp(i, j + 1, k + 1, sum + nums[-(j+1)] * multiply[k]))
+            return memo[i, j, k]
+
+    ans = dp(0, 0, 0, 0)
+    return ans
+     */
 }
