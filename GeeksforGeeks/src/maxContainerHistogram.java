951dab541ec89af8cfaf689a03ed51ef4f28084b
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-01 08:13:22 -0500

    previous approch was wrong, becuz you cannot skip previous one becuz it is smaller than current one. This approach is maximizing the h*w.

--- GeeksforGeeks/src/maxContainerHistogram.java
+++ GeeksforGeeks/src/maxContainerHistogram.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 public class maxContainerHistogram {
-    public static void main(String[] args)
-    {
-        System.out.print(maxArea(new int[]{1,5,4,3}, 4));
-    }
-    static long maxArea(int[] bar, int n) {
-        Stack<Integer> s = new Stack<>();
-
+    // Container With Most Water:
+        // Because the length is always decreasing
+        // we only need to consider a pair if its height
+        // is greater than any pair's height we've used so far
+    public int maxArea(int[] h) {
+        int n = h.length;
         int maxArea = 0;
-        for(int i = 0; i < n; i++)
-        {
-            while(!s.isEmpty() && bar[i] > bar[s.peek()])
-                maxArea = Math.max(maxArea,
-                        Math.abs((i-s.peek())*bar[s.pop()]));
-            s.add(i);
-        }
-        Queue<Integer> q = new LinkedList<>();
-        int maxIdx = -1;
-        while(!s.isEmpty()) {
-            maxIdx = s.peek();
-            q.add(s.pop());
-        }
-        while(!q.isEmpty()) {
-            int max = q.remove();
-            maxArea = Math.max(maxArea,
-                    Math.abs((max-maxIdx)*bar[max]));
+        int i = 0, j = n - 1;
+        HashSet<Integer> usedH = new HashSet<>();
+        int minUsedH = -1;
+        while (i < j) {
+            // Time limit:
+            // for(int uh : usedH)
+            // {
+            //     while(i < j && h[i] <= uh) i++;
+            //     while(i < j && h[j] <= uh) j--;
+            // }
+            // if(i > j) break;
+
+            if (h[i] > minUsedH && h[j] > minUsedH) {
+                minUsedH = Math.min(h[i], h[j]);
+                maxArea = Math.max(maxArea, (j - i) * minUsedH);
+            }
+            if (h[i] == h[j]) {
+                i++;
+                j--;
+            }
+            else if (h[i] < h[j]) i++;
+            else if (h[i] > h[j]) j--;
         }
 
         return maxArea;
