fee458e89b1d25a38b60c477e98178e72813e435
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-03 20:15:11 -0500

    LeetCode/src/cloneListRandomPointer.java

---
+++ LeetCode/src/cloneListRandomPointer.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class cloneListRandomPointer {
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        var map = new HashMap<Node, Node>();
+
+        // loop 1. copy all the nodes
+        Node node = head;
+        while (node != null) {
+            map.put(node, new Node(node.val));
+            node = node.next;
+        }
+
+        // loop 2. assign next and random pointers
+        node = head;
+        while (node != null) {
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+
+        return map.get(head);
+    }
+}
