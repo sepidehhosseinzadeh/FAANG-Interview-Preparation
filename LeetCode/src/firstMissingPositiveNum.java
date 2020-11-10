e3cd4df2dccbce23a2762620a68490a0b43c3d3c
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-10 10:54:22 -0500

    added another approach

--- LeetCode/src/firstMissingPositiveNum.java
+++ LeetCode/src/firstMissingPositiveNum.java
@@ -7,7 +7,8 @@ public class firstMissingPositiveNum {
     }
 
     // 1 - using xor of nums 1 to n with nums
-    // using HashSet
+    // 2- using HashSet
+    // 3-
     public static int firstMissingPositive(int[] nums) {
         int n = nums.length;
 
@@ -23,5 +24,23 @@ public class firstMissingPositiveNum {
                 return i + 1;
 
         return n + 1;
+    }
+    
+    // 4-
+    int firstMissingPositive(int A[], int n)
+    {
+        for (int i = 0; i < n; ++i)
+            while (A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
+                swap(A[i], A[A[i] - 1], A);
+
+        for (int i = 0; i < n; ++i)
+            if (A[i] != i + 1)
+                return i + 1;
+
+        return n + 1;
+    }
+    void swap(int i, int j, int[] a)
+    {
+        int t = a[i]; a[i] = a[j]; a[j] = t;
     }
 }
