217c36f32ff2d7a6f470983a7b3dbca4d85f2430
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-11 07:58:32 -0500

    GeeksforGeeks/src/topKnumStream.java new approach

--- GeeksforGeeks/src/topKnumStream.java
+++ GeeksforGeeks/src/topKnumStream.java
@@ -3,16 +3,44 @@ import java.lang.*;
 
 // Top k numbers in a stream: print all distinct elements sorted by frequency.
 class topKnumStream {
+
+    static ArrayList<Integer> kTop(int[] a, int n, int k)
+    {
+        var res = new ArrayList<Integer>();
+        var count = new HashMap<Integer, Integer>();
+        var q = new PriorityQueue<Integer>((i,j) ->
+                count.get(i)!=count.get(j)?count.get(j)-count.get(i):i-j);
+
+        for(int i = 0; i < n; i++)
+        {
+            int num = a[i];
+
+            int cnt = count.getOrDefault(num,0);
+            if(cnt > 0) q.remove(num);
+            count.put(num, cnt+1);
+            q.add(num);
+
+            int size = Math.min(q.size(),k);
+            var tmp = new ArrayList<Integer>();
+            for(int j = 0; j < size; j++)
+            {
+                int cur = q.remove();
+                res.add(cur);
+                tmp.add(cur);
+            }
+            for(int e : tmp) q.add(e);
+        }
+
+        return res;
+    }
+
+    // prev approach
     static class Node implements Comparable<Node>
     {
         int val, cnt;
-        Node(int v, int c)
-        {
-            val = v; cnt = c;
-        }
+        Node(int v, int c) {val = v; cnt = c;}
         @Override
-        public int compareTo(Node o)
-        {
+        public int compareTo(Node o) {
             return this.cnt != o.cnt ?
                     o.cnt - this.cnt : this.val - o.val;
         }
@@ -23,7 +51,6 @@ class topKnumStream {
                     cnt == node.cnt;
         }
     }
-
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
@@ -32,14 +59,12 @@ class topKnumStream {
         while(t-- > 0)
         {
             final int n = sc.nextInt(), k = sc.nextInt();
-
             PriorityQueue<Node> q = new PriorityQueue<>();
             HashMap<Integer, Integer> count = new HashMap<>();
 
             for(int i = 0; i < n; i++)
             {
                 int num = sc.nextInt();
-
                 int cnt = count.getOrDefault(num,0);
                 if(cnt > 0)
                     q.remove(new Node(num, cnt));
