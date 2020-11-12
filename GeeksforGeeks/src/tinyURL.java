618a38913e4f6d3fadc7c9fd0af7b836b68bfd77
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-11 19:35:40 -0500

    GeeksforGeeks/src/tinyURL.java

---
+++ GeeksforGeeks/src/tinyURL.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.util.*;
+
+// https://leetcode.com/discuss/interview-question/124658/Design-a-URL-Shortener-(-TinyURL-)-System/
+public class tinyURL {
+    // Function to generate a short url from integer ID
+    static String idToShortURL(int n)
+    {
+        // Map to store 62 possible characters
+        char map[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+
+        StringBuffer shorturl = new StringBuffer();
+
+        // Convert given integer id to a base 62 number
+        while (n > 0)
+        {
+            // use above map to store actual character
+            shorturl.append(map[n % 62]);
+            n = n / 62;
+        }
+
+        // Reverse shortURL to complete base conversion
+        return shorturl.reverse().toString();
+    }
+
+    // Function to get integer ID back from a short url
+    static int shortURLtoID(String shortURL)
+    {
+        int id = 0;
+
+        // A simple base conversion logic
+        for (int i = 0; i < shortURL.length(); i++)
+        {
+            if ('a' <= shortURL.charAt(i) &&
+                    shortURL.charAt(i) <= 'z')
+                id = id * 62 + shortURL.charAt(i) - 'a';
+            if ('A' <= shortURL.charAt(i) &&
+                    shortURL.charAt(i) <= 'Z')
+                id = id * 62 + shortURL.charAt(i) - 'A' + 26;
+            if ('0' <= shortURL.charAt(i) &&
+                    shortURL.charAt(i) <= '9')
+                id = id * 62 + shortURL.charAt(i) - '0' + 52;
+        }
+        return id;
+    }
+
+    public static void main (String[] args) throws IOException
+    {
+        int n = 12345;
+        String shorturl = idToShortURL(n);
+        System.out.println("Generated short url is " + shorturl);
+        System.out.println("Id from url is " +
+                shortURLtoID(shorturl));
+    }
+}
