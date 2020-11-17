a5493196646f0933969e1678e5b66c44b81d3b93
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-17 15:29:47 -0500

    LeetCode/src/distributeNumbers.java added a correct backtrack approach

--- LeetCode/src/distributeNumbers.java
+++ LeetCode/src/distributeNumbers.java
@@ -37,7 +37,7 @@ public class distributeNumbers {
 
     // for each num, assign a subset f orders (2^m) -> O((2^m)^n) TLE!!!
     // it should be bottom up! it's NOT using memo!
-    boolean[][] memo;
+    int[][] memo;
     int n, m;
     public boolean canDistribute_v1(int[] nums, int[] quantity) {
         var map = new HashMap<Integer, Integer>();
@@ -55,7 +55,8 @@ public class distributeNumbers {
                 if((1<<j & i) != 0)
                     required[i] += quantity[j];
 
-        memo = new boolean[n][1<<m];
+        memo = new int[n][1<<m];
+        for(int[] i : memo) Arrays.fill(i,-1);
 
         return canAssign_v1(0, 0, cnt, required);
     }
@@ -63,20 +64,66 @@ public class distributeNumbers {
     {
         if(selectedMask == (1<<m)-1) return true; // all customers are served
         if(at == n) return false;
-        if(memo[at][selectedMask]) return memo[at][selectedMask];
+        if(memo[at][selectedMask]!=-1) return memo[at][selectedMask]==1;
 
         // potential candidates to be assigned to cnt[at]
         for(int i = 0; i < 1<<m; i++)
             if((selectedMask & i) == 0)
                 if(required[i] <= cnt[at])
                     if(canAssign_v1(at+1, selectedMask | i, cnt, required))
-                        return memo[at][selectedMask] = true;
+                    {
+                        memo[at][selectedMask] = 1;
+                        return true;
+                    }
 
         return false;
     }
 
     // bottom-up
+    public boolean canDistribute_v1_1(int[] nums, int[] quantity) {
+        var map = new HashMap<Integer, Integer>();
+        for (int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
+
+        int j = 0;
+        int[] cnt = new int[map.size()];
+        for (int i : map.values())
+            cnt[j++] = i;
+
+        n = cnt.length; m = quantity.length;
+        int[] required = new int[1<<m];
+        for(int i = 0; i < 1<<m; i++)
+            for(j = 0; j < m; j++)
+                if((1<<j & i) != 0)
+                    required[i] += quantity[j];
+
+        memo = new int[n][1<<m];
+        for(int[] i : memo) Arrays.fill(i,-1);
+
+        return canAssign_v1_1(0, 0, cnt, required);
+    }
+    boolean canAssign_v1_1(int at, int selectedMask, int[] cnt, int[] required)
+    {
+        if(selectedMask == (1<<m)-1) return true; // all customers are served
+        if(at == n) return false;
+        if(memo[at][selectedMask] != -1)
+            return memo[at][selectedMask]==1;
+
+        // in order to use memo (sub-problems), we need a bottom-up call
+        // or for on subsets of selectedMask
+        boolean res = canAssign_v1_1(at+1, selectedMask, cnt, required);
+
+        // potential candidates to be assigned to cnt[at]
+        for(int i = 0; i < (1<<m); i++)
+            if(selectedMask == (selectedMask & i)) { // keep all previous 1s in selectedMask (already served customers)
+                if(required[(~selectedMask)&i] <= cnt[at]) // if 1s in i and not in selectedMask can be served by at
+                    res |= canAssign_v1_1(at+1, i, cnt, required);
+                if(res) break;
+            }
+
+        memo[at][selectedMask] = (res?1:0);
+        return res;
+    }
+
+    // DP
     
-
-
 }
