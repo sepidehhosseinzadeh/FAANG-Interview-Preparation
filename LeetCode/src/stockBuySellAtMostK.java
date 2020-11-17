7612934c1c0e05bd5b4993eb4442d7574f149bde
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-17 17:39:19 -0500

    LeetCode/src/stockBuySellAtMostK.java more optimized

--- LeetCode/src/stockBuySellAtMostK.java
+++ LeetCode/src/stockBuySellAtMostK.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class stockBuySellAtMostK {
     // dp[k, i] = max(dp[k, i-1], prices[i] - prices[j] + dp[k-1, j-1]), j=[0..i-1]
+    // O(kn^2)
     public int maxProfit(int K, int[] prices) {
         int n = prices.length;
         if(n == 0) return 0;
@@ -16,6 +17,23 @@ public class stockBuySellAtMostK {
                     maxProf[k][i] = Math.max(maxProf[k][i],
                             prices[i]-prices[j]+(j>=1?maxProf[k-1][j-1]:0));
             }
+        return maxProf[K][n-1];
+    }
+    // O(kn)
+    public int maxProfit_v1(int K, int[] prices) {
+        int n = prices.length;
+        if(n == 0) return 0;
+        int[][] maxProf = new int[K+1][n]; // maxProf[i]: max profit till i
+
+        for(int k = 1; k <= K; k++)
+        {
+            int min = prices[0];
+            for(int i = 1; i < n; i++) { // sell
+                maxProf[k][i] = maxProf[k][i-1]; // don't sell at i
+                min = Math.min(min, prices[i]-maxProf[k-1][i-1]); // buy at j < i.
+                maxProf[k][i] = Math.max(maxProf[k][i], prices[i]-min);
+            }
+        }
         return maxProf[K][n-1];
     }
 }
