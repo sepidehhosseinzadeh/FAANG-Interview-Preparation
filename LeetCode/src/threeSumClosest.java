1d3804af1da35370ae5faca2a389630ea69d7f67
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-20 22:41:13 -0500

    LeetCode/src/threeSumClosest.java

--- LeetCode/src/threeSumClosest.java
+++ LeetCode/src/threeSumClosest.java
@@ -1,8 +1,26 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class threeSumClosest 
-{
-    public static void main(String[] args)
-    {
+public class threeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        int res = Integer.MAX_VALUE;
+        int dis = res;
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i + 2 < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip uplicates
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                if (Math.abs(target - (nums[i] + nums[l] + nums[r])) < dis) {
+                    dis = Math.abs(target - (nums[i] + nums[l] + nums[r]));
+                    res = nums[i] + nums[l] + nums[r];
+                }
+                if (nums[i] + nums[l] + nums[r] == target) return res;
+                if (nums[i] + nums[l] + nums[r] > target) r--;
+                else l++;
+            }
+        }
+
+        return res;
     }
 }
