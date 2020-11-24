0232a184672e2ba4e104290636c10c0a6160bf65
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-23 21:20:48 -0500

    LeetCode/src/bitCnt1n.java

--- LeetCode/src/bitCnt1n.java
+++ LeetCode/src/bitCnt1n.java
@@ -1,8 +1,11 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class bitCnt1n 
-{
-    public static void main(String[] args)
-    {
+public class bitCnt1n {
+    public int[] countBits(int num) {
+        int[] bitCnt = new int[num+1];
+        for(int i = 0; i <= num; i++)
+            bitCnt[i] = bitCnt[i >> 1] + (i&1);
+        //bitCnt[i] = bitCnt[i/2]+i%2;
+        return bitCnt;
     }
 }
