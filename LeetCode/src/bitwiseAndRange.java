fa5edc823b090de7822a0ba279c66c398d92dea6
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-23 21:21:45 -0500

    LeetCode/src/bitwiseAndRange.java

--- LeetCode/src/bitwiseAndRange.java
+++ LeetCode/src/bitwiseAndRange.java
@@ -1,8 +1,14 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class bitwiseAndRange 
-{
-    public static void main(String[] args)
-    {
+public class bitwiseAndRange {
+    public int rangeBitwiseAnd(int m, int n) {
+        int lenOfZero = 0;
+        while(m != n) // until they are not equal, shift right
+        {
+            m >>= 1;
+            n >>= 1;
+            lenOfZero++;
+        }
+        return m << lenOfZero; // the right part that are not equal should be zero
     }
 }
