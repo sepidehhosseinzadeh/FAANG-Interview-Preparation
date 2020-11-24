11cc2d8e2dc3951187e0d110ef317c2b77698d27
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-23 21:21:06 -0500

    LeetCode/src/bitReverse.java

--- LeetCode/src/bitReverse.java
+++ LeetCode/src/bitReverse.java
@@ -1,8 +1,24 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class bitReverse 
-{
-    public static void main(String[] args)
-    {
+public class bitReverse {
+    // you need treat n as an unsigned value
+    public int reverseBits_(int n) {
+        int mask = 1 << 31, res = 0;
+        for (int i = 0; i < 32; ++i) {
+            if ((n & 1) != 0) res |= mask;
+            mask >>= 1;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    public int reverseBits(int n) {
+        int mask = 1, res = 0;
+        for (int i = 0; i < 32; ++i) {
+            res <<= 1;
+            if ((mask & n) != 0) res |= 1;
+            mask <<= 1;
+        }
+        return res;
     }
 }
