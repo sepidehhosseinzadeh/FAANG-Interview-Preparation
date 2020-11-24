0c1f09c8eeca7d22876c4a194411f17258ecd6b3
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-23 21:22:40 -0500

    LeetCode/src/minCoinChange.java

--- LeetCode/src/minCoinChange.java
+++ LeetCode/src/minCoinChange.java
@@ -1,8 +1,20 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class minCoinChange 
-{
-    public static void main(String[] args)
-    {
+public class minCoinChange {
+    public int coinChange(int[] coins, int m) {
+        int n = coins.length;
+        var ways = new int[m+1];
+        Arrays.fill(ways, Integer.MAX_VALUE/2);
+
+        ways[0] = 0;
+
+        for(int i = 1; i <= m; i++)
+            for(int j = 0; j < n; j++)
+                if(i >= coins[j])
+                    ways[i] = Math.min(ways[i],
+                            ways[i-coins[j]]+1);
+
+        return ways[m] >= Integer.MAX_VALUE/2 ? -1 : ways[m];
+
     }
 }
