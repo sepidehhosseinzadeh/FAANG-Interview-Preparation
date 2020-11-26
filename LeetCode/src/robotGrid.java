4326619be464256e8f46a251e7fe59b195bbe781
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-11-26 08:43:46 -0500

    LeetCode/src/robotGrid.java

--- LeetCode/src/robotGrid.java
+++ LeetCode/src/robotGrid.java
@@ -1,8 +1,58 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class robotGrid 
-{
-    public static void main(String[] args)
-    {
+public class robotGrid {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+        return dp[m - 1][n - 1];
+    }
+    // space opt
+    /*dp[j] += dp[j - 1]; is
+        dp[j] = dp[j] + dp[j - 1];
+        which is new dp[j] = old dp[j] + dp[j-1]
+        which is current cell = top cell + left cell
+
+    int uniquePaths(int m, int n) {
+        vector<int> pre(n, 1), cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] = pre[j] + cur[j - 1];
+            }
+            swap(pre, cur);
+        }
+        return pre[n - 1];
+    }
+    int uniquePaths(int m, int n) {
+        vector<int> cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+    Now, can we further reduce the memory usage to just O(1) space
+    since the above code seems to use only two variables (cur[j] and cur[j - 1])?
+    However, since the whole row cur needs to be updated for m - 1 times (the outer loop)
+    based on old values, all of its values need to be saved and thus O(1)-space is impossible
+    */
+    int uniquePaths_v_math(int m, int n) {
+        int N = n + m - 2;// how much steps we need to do
+        int k = m - 1; // number of steps that need to go down
+        double res = 1;
+        // here we calculate the total possible path number
+        // Combination(N, k) = n! / (k!(n - k)!)
+        // reduce the numerator and denominator and get
+        // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
+        for (int i = 1; i <= k; i++)
+            res = res * (N - k + i) / i;
+        return (int) res;
     }
 }
+
