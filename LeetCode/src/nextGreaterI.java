5fa827f567e71977c01af3162d1c28cf660083af
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-02 20:14:26 -0500

    LeetCode/src/nextGreaterI.java

--- LeetCode/src/nextGreaterI.java
+++ LeetCode/src/nextGreaterI.java
@@ -1,8 +1,51 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class nextGreaterI 
-{
-    public static void main(String[] args)
-    {
+public class nextGreaterI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        var res = new HashMap<Integer, Integer>();
+        var stack = new Stack<Integer>();
+
+        for(int i = 0; i < m; i++) {
+            // all the elem in stack (previous elem) now see a greater num
+            while(!stack.isEmpty() && stack.peek() < nums2[i])
+                res.put(stack.pop(), nums2[i]);
+
+            stack.push(nums2[i]);
+        }
+
+        int[] ret = new int[n];
+        int i = 0;
+        for(int v : nums1)
+            ret[i++] = res.getOrDefault(v, -1);
+
+        return ret;
+
+    }
+    public int[] nextGreaterElement_v0(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        var res = new HashMap<Integer, Integer>();
+        var stack = new Stack<Integer>();
+
+        for(int i = m-1; i >= 0; i--) {
+            while(!stack.isEmpty() && stack.peek() < nums2[i]) {
+                int cur = stack.pop();
+                res.put(cur, !stack.isEmpty() ? stack.peek() : -1);
+            }
+            stack.push(nums2[i]);
+        }
+
+        while(!stack.isEmpty()) {
+            int cur = stack.pop();
+            res.put(cur, !stack.isEmpty() ? stack.peek() : -1);
+        }
+
+        int[] ret = new int[n];
+        int i = 0;
+        for(int v : nums1)
+            ret[i++] = res.get(v);
+
+        return ret;
+
     }
 }
