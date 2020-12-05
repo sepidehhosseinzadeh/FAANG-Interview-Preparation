fd71be1f7db48145b97f1908b5a45ffab7ebb9e8
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-04 23:26:23 -0500

    LeetCode/src/intervalMinDelToHaveNonOverlap.java

--- LeetCode/src/intervalMinDelToHaveNonOverlap.java
+++ LeetCode/src/intervalMinDelToHaveNonOverlap.java
@@ -1,8 +1,19 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class intervalMinDelToHaveNonOverlap 
-{
-    public static void main(String[] args)
-    {
-    }
+public class intervalMinDelToHaveNonOverlap {
+	public int eraseOverlapIntervals(int[][] in) {
+		if(in.length == 0) return 0;
+		Arrays.sort(in, (i, j) -> i[0]!=j[0] ? i[0]-j[0] : i[1]-j[1]);
+		int cnt = 0;
+		int ub = in[0][1];
+
+		for(int i = 1; i < in.length; i++) {
+			if(ub > in[i][0]) { // overlap
+				cnt++;
+				ub = Math.min(ub, in[i][1]);
+			} else
+				ub = Math.max(ub, in[i][1]);
+		}
+		return cnt;
+	}
 }
