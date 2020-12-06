ba0e113a70195cd7bb3c3475f28c7ef5edefe069
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-05 23:30:18 -0500

    LeetCode/src/decode.java

--- LeetCode/src/decode.java
+++ LeetCode/src/decode.java
@@ -1,8 +1,22 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class decode 
-{
-    public static void main(String[] args)
-    {
-    }
+public class decode {
+	public String interpret(String command) {
+		return command.replace("()", "o").replace("(", "").replace(")", "");
+	}
+	public String interpret_v1(String s) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == 'G') sb.append('G');
+			else if(s.charAt(i) == '(' && s.charAt(i + 1) == ')') {
+				i++;
+				sb.append('o');
+			} else {
+				i += 3;
+				sb.append("al");
+			}
+
+		}
+		return sb.toString();
+	}
 }
