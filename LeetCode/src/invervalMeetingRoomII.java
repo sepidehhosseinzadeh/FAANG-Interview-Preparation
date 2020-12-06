d70cc911cedbb8f14e1bac0f9741f5daeba118d7
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-05 23:30:35 -0500

    LeetCode/src/invervalMeetingRoomII.java

--- LeetCode/src/invervalMeetingRoomII.java
+++ LeetCode/src/invervalMeetingRoomII.java
@@ -13,13 +13,25 @@ public class invervalMeetingRoomII {
 		int cnt = 0, max = 0;
 		for (int i : t) {
 			cnt += i;
-			max = Math.max(cnt, max);
+			max = Math.max(cnt, max); // we need to find the most overlapped intervals
 		}
 
 		return max;
 	}
+	public int minMeetingRooms_v1(int[][] intervals) {
+		Map<Integer, Integer> map = new TreeMap<>();
+		for (int[] itl : intervals) {
+			map.put(itl[0], map.getOrDefault(itl[0], 0) + 1);
+			map.put(itl[1], map.getOrDefault(itl[1], 0) - 1);
+		}
+		int room = 0, k = 0;
+		for (int v : map.values())
+			k = Math.max(k, room += v);
 
-	public int minMeetingRooms(int[][] in) {
+		return k;
+	}
+
+	public int minMeetingRooms_v2(int[][] in) {
 		int n = in.length;
 		if (n == 0) return 0;
 
@@ -41,22 +53,43 @@ public class invervalMeetingRoomII {
 				add++;
 			}
 			i++;
-			max = Math.max(max, add);
+			//max = Math.max(max, add); no need for max!!
 		}
 
-		return max;
+		return add;
+	}
+	public int minMeetingRooms_v3(int[][] in) {
+		int n = in.length;
+		int[] starts = new int[n];
+		int[] ends = new int[n];
+		for(int i=0; i<n; i++) {
+			starts[i] = in[i][0];
+			ends[i] = in[i][1];
+		}
+		Arrays.sort(starts);
+		Arrays.sort(ends);
+		int rooms = 0;
+		int endsItr = 0;
+		for(int i=0; i<starts.length; i++) {
+			if(starts[i]<ends[endsItr])
+				rooms++;
+			else
+				endsItr++;
+		}
+		return rooms;
 	}
 
-	// !!!!
-    /*public int minMeetingRooms_v1(Interval[] intervals) {
-        Arrays.sort(intervals,(a,b)->(a.start-b.start));
-        PriorityQueue<Interval> pq=new PriorityQueue<>((a,b)->(a.end-b.end));
-        for(Interval i:intervals){
-            if(!pq.isEmpty()&&pq.peek().end<=i.start){
-                pq.poll();
-            }
-            pq.add(i);
-        }
-        return pq.size();
-    }*/
+	// heap
+	public int minMeetingRooms(int[][] in) {
+		Arrays.sort(in, (i, j) -> i[0] - j[0]); // sort based on start. I see the meetings based on start time, and add room
+		var q = new PriorityQueue<int[]>((i, j) -> i[1] - j[1]); // q is based on end. I remove meetings from q based on end time!
+
+		for (int i = 0; i < in.length; i++) {
+			if (!q.isEmpty() && q.peek()[1] <= in[i][0])
+				q.poll();
+			q.offer(in[i]);
+		}
+
+		return q.size();
+	}
 }
