888d21a642e41684ca5cf8eff93468a899764cc3
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-06 22:38:12 -0500

    LeetCode/src/LinkedListReverse.java

--- LeetCode/src/LinkedListReverse.java
+++ LeetCode/src/LinkedListReverse.java
@@ -11,22 +11,9 @@ public class LinkedListReverse {
 	 * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	 * }
 	 */
-	public ListNode reverseList(ListNode cur) {
-		return reverse(null, cur);
-	}
-	ListNode reverse(ListNode prev, ListNode cur) {
-		if (cur == null) return prev;
-
-		ListNode next = cur.next;
-
-		cur.next = prev; // reverse cur and prev link
-
-		return reverse(cur, next);
-	}
 
 	// recursive :
-	/*
-	 Assume that the rest of the list had already been reversed, now how do I reverse the front part?
+	/* Assume that the rest of the list had already been reversed, now how do I reverse the front part?
 	 Let's assume the list is:
 	n1 → … → nk-1 → nk → nk+1 → … → nm → Ø
 	 Assume from node nk+1 to nm had been reversed and you are at node nk.
@@ -36,7 +23,7 @@ public class LinkedListReverse {
 
 	*n1's next must point to Ø. linklist has a cycle in it -> test linklist of size 2.
 	*/
-	public ListNode reverseList_v1(ListNode cur) {
+	public ListNode reverseList(ListNode cur) {
 		if (cur == null || cur.next == null) //if there's non/1 node reversed is itself
 			return cur;
 
@@ -50,6 +37,18 @@ public class LinkedListReverse {
 		// return reveresd including me
 		return reversed;
 	}
+
+	public ListNode reverseList_v1(ListNode cur) {
+		return reverse(null, cur);
+	}
+	ListNode reverse(ListNode prev, ListNode cur) {
+		if (cur == null) return prev;
+
+		ListNode next = cur.next;
+		cur.next = prev; // reverse cur and prev link
+		return reverse(cur, next);
+	}
+
 
 	// itertative
 	public ListNode reverseList_v0(ListNode head) {
