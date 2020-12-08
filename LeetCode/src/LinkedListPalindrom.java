a20fd98ca90bed1676b0c32677ee1d861d31c8ea
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-08 00:49:53 -0500

    LeetCode/src/LinkedListPalindrom.java

--- LeetCode/src/LinkedListPalindrom.java
+++ LeetCode/src/LinkedListPalindrom.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class LinkedListPalindrom {
-	public boolean isPalindrome(ListNode head) {
+	public boolean isPalindrome_v0(ListNode head) {
 		if(head == null || head.next == null) return true;
 
 		// find middle node: slow
@@ -11,6 +11,7 @@ public class LinkedListPalindrom {
 			fast = fast.next.next;
 		}
 
+		// reverse till midle
 		boolean reversed = false;
 		ListNode t = head, prev = null;
 		while(t != slow) {
@@ -26,5 +27,34 @@ public class LinkedListPalindrom {
 			prev = prev.next;
 		}
 		return true;
+	}
+
+	class node {
+		ListNode val = null;
+		boolean palindrome = true; // is palin till this node
+		node(ListNode v, boolean p) {
+			val = v; palindrome = p;
+		}
+	}
+	// recursive
+	public boolean isPalindrome(ListNode head) {
+		// find len
+		int len = 0;
+		for(ListNode p = head; p != null; p = p.next, len++);
+
+		return isPalin(head, len).palindrome;
+	}
+	node isPalin(ListNode head, int len) {
+		if(head == null || len <= 0) return new node(head, true);
+		if(len == 1) return new node(head.next, true);
+
+		node cur = isPalin(head.next, len-2); // stops at middle
+
+		if(!cur.palindrome || cur.val == null) return cur;
+
+		cur.palindrome = cur.val.val == head.val;
+		cur.val = cur.val.next;
+
+		return cur;
 	}
 }
