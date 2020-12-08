d2ed1f99c722b81cab8fbbd4da57d5b007bbcf94
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-08 00:49:34 -0500

    LeetCode/src/LinkedListRmNfromLast.java

--- LeetCode/src/LinkedListRmNfromLast.java
+++ LeetCode/src/LinkedListRmNfromLast.java
@@ -1,8 +1,22 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class LinkedListRmNfromLast 
-{
-    public static void main(String[] args)
-    {
-    }
+public class LinkedListRmNfromLast {
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if(head == null || head.next == null) return null;
+
+		ListNode h = new ListNode(0);
+		h.next = head;
+		ListNode l = h, r = h;
+		for(int i = 0; i <= n; i++) {
+			r = r.next;
+			if(r == null && i < n-1) return null;
+		}
+
+		while(r != null) {
+			l = l.next;
+			r = r.next;
+		}
+		l.next = l.next.next;
+		return h.next;
+	}
 }
