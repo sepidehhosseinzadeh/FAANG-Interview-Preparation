8a23fa235a2554b2ef4d420bdccc7bb3ad027a0a
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-11 12:47:22 -0500

    another approach LeetCode/src/minWinSubStr.java

--- LeetCode/src/minWinSubStr.java
+++ LeetCode/src/minWinSubStr.java
@@ -5,6 +5,44 @@ import java.util.*;
 // find the minimum window in S which will contain all the characters in T.
 
 public class minWinSubStr {
+    public String minWindow_(String ss, String tt) {
+        int n = ss.length();
+        char[] s = ss.toCharArray();
+        var t = new HashMap<Character, Integer>();
+        var win =  new HashMap<Character, Integer>();
+
+        for(char c : tt.toCharArray()) t.put(c, t.getOrDefault(c,0)+1);
+
+        int i = 0, j = 0, len = 0;
+        int minLen = Integer.MAX_VALUE, I = -1, J = -1;
+
+        while(i < n && j < n) {
+            win.put(s[j], win.getOrDefault(s[j],0)+1);
+            if(t.containsKey(s[j]) && win.get(s[j]) <= t.get(s[j])) len++;
+
+            while(i<n && win.containsKey(s[i]) &&
+                    (!t.containsKey(s[i]) || win.get(s[i]) > t.get(s[i]))) {
+                win.put(s[i], win.get(s[i])-1);
+
+                if(t.containsKey(s[i]) && win.get(s[i]) < t.get(s[i])) len--;
+
+                if(win.get(s[i]) == 0) win.remove(s[i]);
+                i++;
+            }
+
+            if(len == tt.length() && j-i+1 < minLen) {
+                minLen = j-i+1;
+                I = i; J = j;
+            }
+            j++;
+        }
+        if(minLen == Integer.MAX_VALUE) return "";
+
+        String res = "";
+        for(int k = I; k <= J; res += s[k], k++);
+
+        return res;
+    }
     public String minWindow(String s, String t) {
         HashMap<Character, Integer> tm = new HashMap<>();
         HashMap<Character, Integer> window = new HashMap<>();
