ec42afc2ff9c3aaf4bfb8561f9d970b13790d5dd
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-11 22:29:27 -0500

    LeetCode/src/validAnagram.java

--- LeetCode/src/validAnagram.java
+++ LeetCode/src/validAnagram.java
@@ -1,8 +1,11 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class validAnagram 
-{
-    public static void main(String[] args)
-    {
-    }
+public class validAnagram {
+	public boolean isAnagram(String s, String t) {
+		int[] cnt = new int[26];
+		for (char c : s.toCharArray()) cnt[c-'a']++;
+		for (char c : t.toCharArray()) cnt[c-'a']--;
+		for (int i : cnt) if (i != 0) return false;
+		return true;
+	}
 }
