db747709541f655a9e875a33e092dbb37426def5
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-11 22:29:12 -0500

    CodeForces/src/countSpruce.java

--- CodeForces/src/countSpruce.java
+++ CodeForces/src/countSpruce.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class countSpruce {
-	public static void main(String[] args)
+	// TLE
+	public static void main_(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
@@ -28,10 +29,52 @@ public class countSpruce {
 								}
 
 							if(ok) cnt++;
+							else break;
 						}
 					}
 
 
+
+			System.out.println(cnt);
+		}
+
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while (t-- > 0) {
+			int n = scanner.nextInt(), m = scanner.nextInt();
+			scanner.nextLine();
+			char[][] mat = new char[n][m];
+			int[][] dp = new int[n][m];
+
+			for(int i = 0; i < n; i++)
+				mat[i] = scanner.nextLine().toCharArray();
+
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < m; j++)
+					if(mat[i][j] == '*') {
+						dp[i][j] = 1;
+						if (j > 0) dp[i][j] += dp[i][j - 1];
+					}
+			int cnt = 0;
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < m; j++)
+					if(mat[i][j] == '*') {
+						cnt++;
+						for (int k = 2; k <= 500; k++)
+							if(i + k - 1 < n && j + k - 1 < m){
+								if (j - k + 1 == 0) {
+									if (dp[i + k - 1][j + k - 1] == 2 * k - 1) cnt++;
+									else break;
+								} else if (j - k + 1 - 1 >= 0)
+									if (dp[i + k - 1][j + k - 1] - dp[i + k - 1][j - k + 1 - 1] == 2 * k - 1) cnt++;
+									else break;
+							}
+
+					}
 
 			System.out.println(cnt);
 		}
---
+++ LeetCode/src/validAnagram.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;import java.util.*;
+
+public class validAnagram 
+{
+    public static void main(String[] args)
+    {
+    }
+}
