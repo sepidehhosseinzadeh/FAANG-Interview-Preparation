0a134c81365d60375f7490ed1c0b70cd845eba6c
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-16 21:37:22 -0500

    LeetCode/src/linkedListIntersectionWOlen.java

--- LeetCode/src/linkedListIntersectionWOlen.java
+++ LeetCode/src/linkedListIntersectionWOlen.java
@@ -1,8 +1,18 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class linkedListIntersectionWOlen 
-{
-    public static void main(String[] args)
-    {
-    }
+public class linkedListIntersectionWOlen {
+	// first iteration when the shorter one reaches the null (end), the longer one is at the
+	// len-difference (diff) node point (meaning that after the diff nodes will reach the end.
+	// So, in second iteration, when the longer node at diff point moves at the same speed with
+	// the shorter one started at the longer head, and when the longer head reaches null,
+	// the shorter one moved diff node. The longer one continues from shorter head when reaches the end,
+	// they will reach the intersection.
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode a = headA, b = headB;
+		while(a != b) {
+			a = a==null? headB : a.next;
+			b = b==null? headA : b.next;
+		}
+		return a;
+	}
 }
