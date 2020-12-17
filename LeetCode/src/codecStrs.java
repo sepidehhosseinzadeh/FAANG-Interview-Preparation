42628622f48a788a1f8c74eb826a337458530b14
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-16 21:41:34 -0500

    LeetCode/src/codecStrs.java another approach

--- LeetCode/src/codecStrs.java
+++ LeetCode/src/codecStrs.java
@@ -23,3 +23,27 @@ public class codecStrs {
 		return ret;
 	}
 }
+class Codec_v0 {
+	// Encodes a list of strings to a single string.
+	public String encode(List<String> strs) {
+		if (strs.size() == 0) return Character.toString((char)258);
+
+		String d = Character.toString((char)257);
+		StringBuilder sb = new StringBuilder();
+		for(String s: strs) {
+			sb.append(s);
+			sb.append(d);
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+	// Decodes a single string to a list of strings.
+	public List<String> decode(String s) {
+		String d = Character.toString((char)258);
+		if (s.equals(d)) return new ArrayList();
+
+		d = Character.toString((char)257);
+		return Arrays.asList(s.split(d, -1));
+	}
+}
