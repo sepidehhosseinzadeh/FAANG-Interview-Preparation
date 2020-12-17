667ae4687e2d0a3c62562c15a2e20ebb0e37d0f2
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-16 21:38:26 -0500

    LeetCode/src/lowestCommonAncestorBST.java

--- LeetCode/src/lowestCommonAncestorBST.java
+++ LeetCode/src/lowestCommonAncestorBST.java
@@ -1,8 +1,24 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class lowestCommonAncestorBST 
-{
-    public static void main(String[] args)
-    {
-    }
+public class lowestCommonAncestorBST {
+	public TreeNode lowestCommonAncestor_v0(TreeNode root, TreeNode p, TreeNode q) {
+		if(root == null) return null;
+
+		if(root.val > p.val && root.val > q.val)
+			return lowestCommonAncestor(root.left,p,q);
+		else if(root.val < p.val && root.val < q.val)
+			return lowestCommonAncestor(root.right,p,q);
+		return root; // lowest p < root < q
+	}
+	// itertative
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		TreeNode at = root;
+
+		while (at != null) {
+			if(at.val < p.val && at.val < q.val) at = at.right;
+			else if(at.val > p.val && at.val > q.val) at = at.left;
+			else return at;
+		}
+		return null;
+	}
 }
