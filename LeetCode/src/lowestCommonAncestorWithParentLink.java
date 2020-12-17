5967a2098a42bc7102e3c332c79ac701b77f814f
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-16 21:39:37 -0500

    LeetCode/src/lowestCommonAncestorWithParentLink.java

--- LeetCode/src/lowestCommonAncestorWithParentLink.java
+++ LeetCode/src/lowestCommonAncestorWithParentLink.java
@@ -1,8 +1,34 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class lowestCommonAncestorWithParentLink 
-{
-    public static void main(String[] args)
-    {
-    }
+public class lowestCommonAncestorWithParentLink {
+	private class Node {
+		public int val;
+		public Node left;
+		public Node right;
+		public Node parent;
+	}
+	// like LinkedList intersection node
+	public Node lowestCommonAncestor_v0(Node p, Node q) {
+		Node a = p, b = q;
+		while(a != b) {
+			a = a==null? q : a.parent;
+			b = b==null? p : b.parent;
+		}
+		return a;
+	}
+
+	// move up until one covers the other one
+	public Node lowestCommonAncestor(Node p, Node q) {
+		if(p == null || q == null) return null;
+		if(p == q) return p;
+		if(cover(p, q)) return p;
+		if(cover(q, p)) return q;
+
+		return lowestCommonAncestor(p.parent, q.parent);
+	}
+	boolean cover(Node p, Node q) {
+		if(p == null) return false;
+		if(p == q) return true;
+		return cover(p.left, q) || cover(p.right, q);
+	}
 }
