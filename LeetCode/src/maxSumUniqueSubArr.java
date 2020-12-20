92ceba0ede6c716945942848ba84f53d15bcedb7
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-19 23:21:46 -0500

    LeetCode/src/maxSumUniqueSubArr.java

--- LeetCode/src/maxSumUniqueSubArr.java
+++ LeetCode/src/maxSumUniqueSubArr.java
@@ -1,8 +1,21 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class maxSumUniqueSubArr 
-{
-    public static void main(String[] args)
-    {
-    }
+public class maxSumUniqueSubArr {
+	public int maximumUniqueSubarray(int[] nums) {
+		var set = new HashSet<Integer>();
+		int sum = 0, max = 0;
+		for(int i = 0, j = 0; j < nums.length;) {
+			if(!set.contains(nums[j])) {
+				set.add(nums[j]);
+				sum += nums[j];
+				max = Math.max(sum, max);
+				j++;
+			} else {
+				set.remove(nums[i]);
+				sum -= nums[i];
+				i++;
+			}
+		}
+		return max;
+	}
 }
