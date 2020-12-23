1fb450698d78eb1a6d8f47f5c40059c949790058
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-22 21:20:12 -0500

    LeetCode/src/linkedListAddII.java

--- LeetCode/src/linkedListAddII.java
+++ LeetCode/src/linkedListAddII.java
@@ -1,8 +1,30 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class linkedListAdd 
-{
-    public static void main(String[] args)
-    {
-    }
+public class linkedListAddII {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		Stack<Integer> s1 = new Stack<>();
+		Stack<Integer> s2 = new Stack<>();
+		while(l1 != null) {
+			s1.push(l1.val);
+			l1 = l1.next;
+		}
+		while(l2 != null) {
+			s2.push(l2.val);
+			l2 = l2.next;
+		}
+
+		int carry = 0;
+		ListNode cur = null;
+		while(!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+			int s = (s1.isEmpty() ? 0 : s1.pop()) +
+					(s2.isEmpty() ? 0 : s2.pop()) + carry;
+
+			ListNode head = new ListNode(s%10);
+			carry = s/10;
+			head.next = cur;
+			cur = head;
+		}
+
+		return cur;
+	}
 }
