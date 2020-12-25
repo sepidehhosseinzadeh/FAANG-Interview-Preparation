97fe1059e03bc724f0308f0e4715ca833c7dbdc8
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-25 18:28:16 -0500

    LeetCode/src/reverseWords.java

--- LeetCode/src/reverseWords.java
+++ LeetCode/src/reverseWords.java
@@ -1,8 +1,22 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class reverseWords 
-{
-    public static void main(String[] args)
-    {
-    }
+public class reverseWords {
+	public String reverseWords(String s) {
+		String[] w = s.split("\\s+");
+		String res = "";
+		int i = 0, j = w.length-1;
+		while(i < j) {
+			if(w[i].trim().equals("")) w[i++]="";
+			else if(w[j].trim().equals("")) w[j--]="";
+			else {
+				String t = w[i];
+				w[i] = w[j];
+				w[j] = t;
+				i++; j--;
+			}
+		}
+		for(String t : w)
+			res += t+" ";
+		return res.trim();
+	}
 }
