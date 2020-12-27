0cafedf3ad6329171527d1588d5775b0c7be70e5
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-27 07:43:45 -0500

    LeetCode/src/pow.java

--- LeetCode/src/pow.java
+++ LeetCode/src/pow.java
@@ -1,8 +1,13 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class pow 
-{
-    public static void main(String[] args)
-    {
-    }
+public class pow {
+	public double myPow(double x, int n) {
+		if(n == -1) return 1.0/x;
+		if(n == 0) return 1;
+		if(n == 1) return x;
+
+		double p = myPow(x, n/2);
+		if(n%2 != 0) return n>0 ? p*p*x : p*p/x;
+		return p*p;
+	}
 }
