ccdf2db493a4e98097600c203215a23908b0cb1d
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2020-12-30 21:42:51 -0500

    LeetCode/src/reverseOnlyWords.java

--- LeetCode/src/reverseOnlyWords.java
+++ LeetCode/src/reverseOnlyWords.java
@@ -1,8 +1,34 @@
-package PACKAGE_NAME;import java.util.*;
+import java.util.*;
 
-public class reverseOnlyWords 
-{
-    public static void main(String[] args)
-    {
-    }
+public class reverseOnlyWords {
+	public void reverseWords_v0(char[] s) {
+		reverse(s, 0, s.length-1);
+		// reverse words
+		for(int i = 0, j; i < s.length; i++) {
+			if(s[i] != ' ') {
+				for(j = i+1; j < s.length && s[j] != ' '; j++);
+				reverse(s, i, j-1);
+				i = j;
+			}
+		}
+
+	}
+
+	public void reverseWords(char[] s) {
+		reverse(s, 0, s.length-1);
+		// reverse words
+		for(int j = 0, i = 0; j <= s.length; j++) {
+			if(j == s.length || s[j] == ' ') {
+				reverse(s, i, j-1);
+				i = j+1;
+			}
+		}
+	}
+
+	void reverse(char[] s, int i, int j) {
+		while(i < j) {
+			char t = s[i]; s[i] = s[j]; s[j] = t;
+			i++; j--;
+		}
+	}
 }
