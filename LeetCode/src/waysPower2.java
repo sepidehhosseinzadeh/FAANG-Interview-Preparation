9c7ab6068990d7bedab68aee121c9e5e3ae21377
sepideh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-03 19:40:43 -0500

    LeetCode/src/waysPower2.java

--- LeetCode/src/waysPower2.java
+++ LeetCode/src/waysPower2.java
@@ -1,12 +1,6 @@
 import java.util.*;
 
 public class waysPower2 {
-	public static void main(String[] args)
-	{
-		System.out.print(countPairs(new int[]{1,1,1,3,3,3,7}));
-		System.out.print(countPairs(new int[]{64,64,64,64,64,64,64,64,64,64,64,64,64,
-				64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64}));
-	}
 	static int mod = (int) (1e9+7);
 
 	// o(n^2) TLE
@@ -49,7 +43,7 @@ public class waysPower2 {
 		}
 		return (int) res;
 	}
-	
+
 	// count the number of ways in place:
 	public int countPairs(int[] d) {
 		var cnt = new HashMap<Integer, Integer>();
@@ -58,7 +52,7 @@ public class waysPower2 {
 		long res = 0;
 		for(int i : cnt.keySet()) {
 			for(int j = 0, p = 1; j <= 21; j++, p*= 2)
-				if(i <= p-i && cnt.containsKey(p-i)) {
+				if(i <= p-i && cnt.containsKey(p-i)) { // i<=p-i: in order to not count twice
 					if(i != p-i)
 						res += 1L*cnt.get(i)*cnt.get(p-i)%mod;  // choose 1 from cnt[i] * choose 1 from cnt[p-i]
 					else if(i+i == p && cnt.get(i) > 1)
