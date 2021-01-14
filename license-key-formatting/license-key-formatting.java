05ec80a13bf12f1970d664bacf25c46b518ce673
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-13 20:03:03 -0500

    Time: 38 ms (33.67%), Space: 39.8 MB (37.58%) - LeetHub

--- license-key-formatting/license-key-formatting.java
+++ license-key-formatting/license-key-formatting.java
@@ -1,10 +1,6 @@
 class Solution {
     public String licenseKeyFormatting(String S, int K) {
-        StringBuilder res = new StringBuilder();
-        for(char ch : S.toCharArray()) {
-            if(ch != '-')
-                res.append(Character.toUpperCase(ch));
-        }
+        StringBuilder res = new StringBuilder(S.replaceAll("-","").toUpperCase());
         for(int i = res.length()-K; i > 0; i-=K)
             res.insert(i, '-');
             
