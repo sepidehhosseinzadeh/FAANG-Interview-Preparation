fa1bfc26511f38d5f60d4ccd785ca82cbcaaf61f
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-15 09:44:31 -0500

    Time: 1 ms (99.88%), Space: 42.2 MB (85.16%) - LeetHub

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int majorityElement(int[] nums) {
+    public int majorityElement_v0(int[] nums) {
         var cnt = new HashMap<Integer, Integer> ();
         
         for(int i : nums) {
@@ -8,5 +8,23 @@ class Solution {
         }
         
         return -1;
+    }
+    
+    // Boyer-Moore Voting - o(n)
+    // counter++ when we see the potential candidate, o.w. counter--.
+    // when counter is 0, then change the potential candidate to current one. 
+    // coz the majority element will apear more than n/2, 
+    // then whenever we encounter a new elemnt and counter--, 
+    // at the end the majority elemnt will remain
+    public int majorityElement(int[] nums) {
+        if(nums.length == 0) return -1;
+        int cnt = 0;
+        int cand = nums[0];
+        for(int i : nums) {
+            if(cnt == 0) cand = i;
+            cnt += i == cand ? 1 : -1;
+        }
+        return cand;
+        
     }
 }
