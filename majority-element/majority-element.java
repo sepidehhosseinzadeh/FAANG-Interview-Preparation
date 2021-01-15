e2565b48b4cb07491c1b595672bd3b6992e33d73
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-14 20:44:36 -0500

    Time: 9 ms (36.92%), Space: 44.2 MB (42.81%) - LeetHub

---
+++ majority-element/majority-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        var cnt = new HashMap<Integer, Integer> ();
+        
+        for(int i : nums) {
+            cnt.put(i, cnt.getOrDefault(i,0)+1);
+            if(cnt.get(i) > nums.length/2) return i;
+        }
+        return -1;
+    }
+}
