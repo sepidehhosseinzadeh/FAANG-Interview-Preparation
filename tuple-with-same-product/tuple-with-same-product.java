2aa9242d3d21f5ad2b036c66d66de17aab81b1a8
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-17 09:07:28 -0500

    Time: 408 ms (50.00%), Space: 67.5 MB (100.00%) - LeetHub

--- tuple-with-same-product/tuple-with-same-product.java
+++ tuple-with-same-product/tuple-with-same-product.java
@@ -1,4 +1,9 @@
 class Solution {
+    // hashmap cnt for counting different pairs for a specific sum
+    // choose(m,2) * 2!*2!*2! different ways of 2 pairs-each pair 2! and 2 pair can switch side. 
+    // nums = [2,3,4,6] : 
+    // (2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+    // (3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2) 
     public int tupleSameProduct(int[] nums) {
         Map<Long, Integer> cnt = new HashMap<>();
 ​
@@ -12,7 +17,8 @@ class Solution {
         int res = 0;
         for(long m : cnt.keySet())
             if(cnt.getOrDefault(m, 0) > 1)
-                res += cnt.getOrDefault(m, 0)*(cnt.getOrDefault(m, 0)-1)/2*8;
+                res += cnt.getOrDefault(m, 0)*(cnt.getOrDefault(m, 0)-1)/2*8; 
+        
         return res;
     }
 }
