5c89394669bb919b8eea01bc0a11f015f61d52da
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-17 09:58:03 -0500

    Update tuple-with-same-product.java

--- tuple-with-same-product/tuple-with-same-product.java
+++ tuple-with-same-product/tuple-with-same-product.java
@@ -21,4 +21,18 @@ class Solution {
         
         return res;
     }
+    
+    public int tupleSameProduct_v0(int[] nums) {
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                ans += 8 * map.getOrDefault(nums[i] * nums[j], 0);
+                map.put(nums[i] * nums[j], map.getOrDefault(nums[i] * nums[j], 0) + 1);
+            }
+        }
+        
+        return ans;
+    }
 }
