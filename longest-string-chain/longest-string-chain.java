1876f273e4f85307484cf4dbc504abd40d955992
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-20 10:15:55 -0500

    Update longest-string-chain.java

--- longest-string-chain/longest-string-chain.java
+++ longest-string-chain/longest-string-chain.java
@@ -1,31 +1,48 @@
 class Solution {
-    int maxLen;
-    
-    public int longestStrChain(String[] words) {
-        var dic = new HashSet<String>();
-        for(String w : words) dic.add(w);
-        maxLen = 0;
-        
-        for(String w : words) {
-            Set<String> res = new HashSet<>();
-            res.add(w);
-            dfs(new StringBuilder(w), dic, res); 
-        }
-        
-        return maxLen;
-    }
-    void dfs(StringBuilder at, HashSet<String> dic, Set<String> res) {
-        maxLen = Math.max(maxLen, res.size());
-        
-        for(int i = 0; i <= at.length(); i++)
-            for(char c='a'; c <= 'z'; c++) {
-                StringBuilder to = new StringBuilder(at);
-                to.insert(i, c);
-                if(dic.contains(to.toString()) && !res.contains(to.toString())) { 
-                    res.add(to.toString());
-                    dfs(to, dic, res);
-                    res.remove(to.toString());
-                }
-            }
-    }
+    // memo is better time
+    int maxLen;
+    public int longestStrChain_v0(String[] words) {
+        var dic = new HashSet<String>();
+        for(String w : words) dic.add(w);
+        maxLen = 0;
+        
+        for(String w : words) {
+            Set<String> res = new HashSet<>();
+            res.add(w);
+            dfs(new StringBuilder(w), dic, res); 
+        }
+        
+        return maxLen;
+    }
+    void dfs(StringBuilder at, HashSet<String> dic, Set<String> res) {
+        maxLen = Math.max(maxLen, res.size());
+        
+        for(int i = 0; i <= at.length(); i++)
+            for(char c='a'; c <= 'z'; c++) {
+                StringBuilder to = new StringBuilder(at);
+                to.insert(i, c);
+                if(dic.contains(to.toString()) && !res.contains(to.toString())) { 
+                    res.add(to.toString());
+                    dfs(to, dic, res);
+                    res.remove(to.toString());
+                }
+            }
+    }
+    
+    // DP 
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (i,j) -> i.length()-j.length());
+        Map<String, Integer> dp = new HashMap<>();
+        int max = 0;
+        for(String w : words) {
+            int best = 1;
+            for(int i = 0; i < w.length(); i++) {
+                String prev = w.substring(0,i)+w.substring(i+1);
+                best = Math.max(best, dp.getOrDefault(prev,0)+1);
+            }
+            dp.put(w, best);
+            max = Math.max(best, max);
+        }
+        return max;
+    }
 }
