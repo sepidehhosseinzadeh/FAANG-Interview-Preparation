f3fcb7835f6234bc11b184c3a2698e79204457ef
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-01-22 20:22:46 -0500

    Update longest-common-prefix.java

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -1,15 +1,24 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0) return "";
-        for(int i = 0; i < strs[0].length(); i++) {
-            if(!same(i, strs)) return strs[0].substring(0,i);
-        }
-        return strs[0];
-    }
-    boolean same(int idx, String[] s) {
-        char c = s[0].charAt(idx);
-        for(int i = 0; i < s.length; i++)
-            if(idx >= s[i].length() || s[i].charAt(idx) != c) return false;
-        return true;
-    }
+    public String longestCommonPrefix_v0(String[] strs) {
+        if(strs.length == 0) return "";
+        for(int i = 0; i < strs[0].length(); i++) {
+            if(!same(i, strs)) return strs[0].substring(0,i);
+        }
+        return strs[0];
+    }
+    boolean same(int idx, String[] s) {
+        char c = s[0].charAt(idx);
+        for(int i = 0; i < s.length; i++)
+            if(idx >= s[i].length() || s[i].charAt(idx) != c) return false;
+        return true;
+    }
+    
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        String pre = strs[0];   
+        for(int i = 1; i < strs.length; i++)
+            while(!strs[i].startsWith(pre)) // indexOf(pre) != 0
+                pre = pre.substring(0, pre.length()-1);
+        return pre;
+    }
 }
