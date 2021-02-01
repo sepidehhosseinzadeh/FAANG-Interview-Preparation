db361b8525dcf031d2bcdd8dd997fcc9893607ad
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-02-01 08:09:57 -0500

    Time: 4 ms (90.11%), Space: 38.8 MB (87.02%) - LeetHub

--- bulls-and-cows/bulls-and-cows.java
+++ bulls-and-cows/bulls-and-cows.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String getHint(String secret, String guess) {
+    public String getHint_v0(String secret, String guess) {
         int[] cnt = new int[10];
         boolean[] match = new boolean[guess.length()];
         int bull = 0, cow = 0;
@@ -14,6 +14,22 @@ class Solution {
                 cnt[guess.charAt(i)-'0']--;
                 cow++;
             }
+        return bull+"A"+cow+"B";
+    }
+    
+    public String getHint(String secret, String guess) {
+        int[] cnt = new int[10];
+        int bull = 0, cow = 0;
+        for(int i = 0; i < secret.length(); i++) {
+            if(secret.charAt(i) == guess.charAt(i)) bull++;
+            else {
+                if(cnt[guess.charAt(i)-'0'] > 0) cow++;
+                if(cnt[secret.charAt(i)-'0'] < 0) cow++;
+                cnt[guess.charAt(i)-'0']--;
+                cnt[secret.charAt(i)-'0']++;
+            }
+        }
+            
         return bull+"A"+cow+"B";
     }
 }
