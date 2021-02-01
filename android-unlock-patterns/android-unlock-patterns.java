5f533b977f52b0ff1b7f14a986721790614ec1fc
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-02-01 07:39:02 -0500

    Update android-unlock-patterns.java

--- android-unlock-patterns/android-unlock-patterns.java
+++ android-unlock-patterns/android-unlock-patterns.java
@@ -8,15 +8,14 @@ class Solution {
         skip[1][9] = skip[9][1] = skip[3][7] = skip[7][3] =
             skip[2][8] = skip[8][2] = skip[4][6] = skip[6][4] = 5;
         int cnt = 0;
-        boolean[] vis = new boolean[10];
-        for(int i = m; i <= n; i++) { 
-            cnt += dfs(1, i, skip, new boolean[10])*4; // 1,3,7,9 (corners) are symmetric
-            cnt += dfs(2, i, skip, new boolean[10])*4; // 2,4,6,8
-            cnt += dfs(5, i, skip, new boolean[10]); // 5
-        }
-        // for(int i = m; i <= n; i++) 
-        //     for(int j = 1; j < 10; j++)
-        //         cnt += dfs(j, i, skip, new boolean[10]);
+        // for(int i = m; i <= n; i++) { 
+        //     cnt += dfs(1, i, skip, new boolean[10])*4; // 1,3,7,9 (corners) are symmetric
+        //     cnt += dfs(2, i, skip, new boolean[10])*4; // 2,4,6,8
+        //     cnt += dfs(5, i, skip, new boolean[10]); // 5
+        // }
+        for(int i = m; i <= n; i++) 
+            for(int j = 1; j < 10; j++)
+                cnt += dfs(j, i, skip, new boolean[10]);
         return cnt;
     }
     private int dfs(int at, int len, int[][] skip, boolean[] vis) {
