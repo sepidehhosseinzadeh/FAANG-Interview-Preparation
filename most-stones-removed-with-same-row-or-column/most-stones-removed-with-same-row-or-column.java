5514b411d6f94e42912f97b78ccfe9de281d74a1
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-02-04 08:41:33 -0500

    Update most-stones-removed-with-same-row-or-column.java

--- most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int removeStones(int[][] s) {
+    // o(n^2)
+    public int removeStones_v0(int[][] s) {
         int n = s.length;
         int[] p = new int[n];
         for(int i = 0; i < n; p[i]=i++);
@@ -23,5 +24,32 @@ class Solution {
     }
     private boolean isNeigh(int[] a, int[] b) {
         return a[0]==b[0] || a[1] == b[1];
+    }
+    
+    
+    // o(n) : stone's cordinates can be union, 
+    // coz if they have a same x or y they are in 
+    // the same component (tree)
+    // we can distinguish x and y by using ~y or y+MAX
+    int cc = 0;
+    public int removeStones(int[][] s) {
+        int n = s.length;
+        var p = new HashMap<Integer, Integer>();
+        
+        for(int[] stone: s) {
+            int p1 = unionFind(p,stone[0]);
+            int p2 = unionFind(p,~stone[1]);
+            if(p1 != p2) {
+                cc--;
+                p.put(p1,p2);
+            }
+        }
+        return n - cc;
+    }
+    private int unionFind(HashMap<Integer, Integer> p, int i) {
+        if(!p.containsKey(i)) {p.put(i,i); cc++;}
+        if(i == p.get(i)) return i;
+        p.put(i, unionFind(p, p.get(i)));    
+        return p.get(i);
     }
 }
