cf22213a411649a9da82cc7f32aa2bc5dd62e13b
Sepideh Hosseinzadeh <sepideh.hosseinzadeh.h@gmail.com>
2021-02-04 12:17:10 -0500

    Time: 28 ms (65.46%), Space: 39.2 MB (56.59%) - LeetHub

---
+++ shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int shortestDistance(int[][] grid) {
+        if(grid.length == 0) return -1;
+        int n = grid.length, m = grid[0].length;
+        
+        int[][] dis = new int[n][m]; // dis[i][j] = sum dis from every 0 to building
+        int[][] reach = new int[n][m]; // count num of buildings reached from 0
+        
+        // min dis that reach is num of buildings
+        int minD = Integer.MAX_VALUE, nB = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) 
+                if(grid[i][j] == 1) {
+                    bfs(i,j, grid, dis, reach);  
+                    nB++;
+                }
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(grid[i][j] == 0 && reach[i][j] == nB)
+                    minD = Math.min(minD, dis[i][j]);
+        
+        return minD == Integer.MAX_VALUE ? -1 : minD;
+    }
+    
+    private void bfs(int x, int y, int[][] g, int[][] dis, int[][] reach) {
+        int n = dis.length, m = dis[0].length;
+        int[] d = new int[] {0,1,0,-1,0};
+        
+        Queue<int[]> q = new LinkedList();
+        q.add(new int[] {x,y});
+
+        boolean[][] vis = new boolean[n][m];
+        vis[x][y] = true;
+        
+        int level = 1;
+        
+        while(!q.isEmpty()) {
+            int nQ = q.size(); // all childs have dis level
+            
+            for(int i = 0; i < nQ; i++) {
+                int[] at = q.poll();
+                for(int k = 0; k < 4; k++) {
+                    int tx = at[0]+d[k], ty = at[1]+d[k+1];
+                    if(tx<0 || ty<0 || tx>=n || ty >=m) continue;
+                    if(g[tx][ty] == 0 && !vis[tx][ty]) {
+                        vis[tx][ty] = true;
+                        q.add(new int[] {tx,ty});
+
+                        dis[tx][ty] += level;
+                        reach[tx][ty]++;
+                    }
+                }
+            }
+            level++;
+        }
+    }
+}
